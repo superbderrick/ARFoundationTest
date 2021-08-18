0491d959b78066f932b5a218b92743027d984696
derrick <kang.derrick@gmail.com>
2021-08-19 08:49:23 +0900

    added basic code

--- kotlin/app/src/main/java/com/example/daily/MainActivity.kt
+++ kotlin/app/src/main/java/com/example/daily/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.daily
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var testValue0 = "hi"
+        var testValue1 = "hi1"
+        var testValue3 = "hi3"
+
+        Log.d("test" , " $(testValue0 == testValue1)"  )
     }
 }
