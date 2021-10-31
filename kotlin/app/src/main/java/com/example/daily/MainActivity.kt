89952011d1aa8bf84c44bc3dac95a4616046609e
derrick <kang.derrick@gmail.com>
2021-10-31 13:52:30 +0900

    added

--- kotlin/app/src/main/java/com/example/daily/MainActivity.kt
+++ kotlin/app/src/main/java/com/example/daily/MainActivity.kt
@@ -14,5 +14,43 @@ class MainActivity : AppCompatActivity() {
         var testValue3 = "hi3"
 
         Log.d("test" , " $(testValue0 == testValue1)"  )
+
+        var intValues = arrayOf(1,2,3,4)
+
+        Log.d("Main","TestValue : " + max(numbers = intValues) )
+
+        val onToFive:IntRange = 1..5
+        val aToE:CharRange = 'a'..'e'
+
+        val seekHelp: ClosedRange<String> = "hell".."help"
+        Log.d("test" , "${seekHelp.contains("hell")}")
+        Log.d("test" , "${seekHelp.contains("help")}")
+
+        for(i in 1..5) {
+            Log.d("test" , "$i")
+        }
+
+        for(ab in 'a'..'e') {
+            Log.d("test" , "$ab")
+        }
+
+        for(downTest in 5.downTo(1)) {
+            Log.d("test" , "$downTest")
+        }
+        for(stepTestI in 1 until 10 step 3) {
+
+        }
+        //3의 배수 or 5의 배수 
+
+
+    }
+
+    fun max(numbers: Array<Int>) : Int {
+        var large = Int.MAX_VALUE
+
+        for(number in numbers) {
+            large = if(number > large) number else large
+        }
+        return large
     }
 }
