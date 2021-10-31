90e6251bed5c181ab57881f9debcc28856b55700
derrick <kang.derrick@gmail.com>
2021-10-31 19:11:49 +0900

    added

---
+++ Algorithm/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+    val progressValue = intArrayOf(90,30,55)
+    val speedValue = intArrayOf(1,30,5)
+
+    val finalValue = solution(progressValue , speedValue)
+
+
+}
+
+fun solution(progresses: IntArray, speeds: IntArray): IntArray {
+
+    var progressStack = progresses
+
+    //var answer = intArrayOf()
+
+
+
+    return progressStack
+}
